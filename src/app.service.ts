32c85474d0a8ba22130816781de99cac0fd5415a
Bakhtiyar Ospanov <b.p.ospanov@gmail.com>
2023-08-22 09:17:36 +0600

    начальная настройка модуля

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
-@Injectable()
+@Injectable
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getUsers() {
+    return [{ id: 1, name: 'Ulbi TV' }];
   }
 }
